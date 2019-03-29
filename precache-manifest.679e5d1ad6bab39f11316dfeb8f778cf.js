6bfd50f27ccc2d4132cd0581d772d431a2ade292
James Wilkinson <jdwilkinson1994@gmail.com>
2019-03-29 10:33:28 +0800

    Updates

---
+++ precache-manifest.679e5d1ad6bab39f11316dfeb8f778cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7fb47d1b0a3a9c661a8b",
+    "url": "/gymlog/static/js/runtime~main.7fb47d1b.js"
+  },
+  {
+    "revision": "6d0d4ec0cca9eda1f7d4",
+    "url": "/gymlog/static/js/main.6d0d4ec0.chunk.js"
+  },
+  {
+    "revision": "d1d5a92b44e99fdeb293",
+    "url": "/gymlog/static/js/2.d1d5a92b.chunk.js"
+  },
+  {
+    "revision": "6d0d4ec0cca9eda1f7d4",
+    "url": "/gymlog/static/css/main.776fffe2.chunk.css"
+  },
+  {
+    "revision": "74f2231102cd1afac83d5b62321143f4",
+    "url": "/gymlog/index.html"
+  }
+];
